b51f5fdf1eb24f298ba22c681ada26adc2b753f6
nurs <nursultanzhantuar@gmail.com>
2024-09-16 21:33:44 +0500

    Finish tasks 9 and 10

--- lab2/9.cpp
+++ lab2/9.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 #include <queue>
 #include <map>
-
 using namespace std;
 
 void test() {
     int n; cin >> n;
-    queue <string> word_queue;
-    map <string, int> word_count;
-    string word;
+
+    queue<char> q;
+    map <char, int> freq;
+    string result;
 
     for (int i = 0; i < n; i++) {
-        cin >> word;
-        if (word == word_queue.front()) {
-            while (word_count[word_queue.front()] > 1) {
-                word_queue.pop();
-            }
-        } else {
-            word_queue.push(word);
-            word_count[word] += 1;
-        }
-    
+        char ch;
+        cin >> ch;
+        freq[ch]++;
+        q.push(ch);
 
-        if (!word_queue.empty()) {
-            cout << word_queue.front() << " ";
+        while (!q.empty() && freq[q.front()] > 1) {
+            q.pop();
+        }
+
+        if (q.empty()) {
+            result += "-1 ";
         } else {
-            cout << -1 << " ";
+            result += q.front();
+            result += " ";
         }
     }
-    cout << endl;
+
+    cout << result << endl;
+
 }
 
 int main() {
