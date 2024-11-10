9bace788247d0158913baf30e41177ff9e353299
nurs <nursultanzhantuar@gmail.com>
2024-11-10 10:04:52 +0500

    Add lab8 task h

--- lab8/h.cpp
+++ lab8/h.cpp
@@ -4,28 +4,79 @@
 
 using namespace std;
 
-const long long int MOD = 1e15 + 9;
-const int PRIME = 31;
+const long long PRIME = 31;
 
-long long count_substrings(string s) {
-    unordered_map<long long, bool> substrings;
-    for (long long i = 0; i < s.size(); i++) {
-        long long hash = 0;
-        for (long long j = i; j < s.size(); j++) {
-            hash = (PRIME * hash + s[j]) % MOD;
-            if (substrings.find(hash) == substrings.end()) {
-                substrings[hash] = true;
-            }
+long long hash_func(string s) {
+    long long h = 0;
+    for (int i = 0; i < s.length(); i++) {
+        h = (h * PRIME + (s[i] - 'a' + 1));
+    }
+    return h;
+}
+
+bool rabinKarp(string text, string pattern) {
+    int n = text.length();
+    int m = pattern.length();
+
+    int text_hash = hash_func(text.substr(0, m));
+    int pattern_hash = hash_func(pattern);
+
+    int prime = 1;
+    for (int i = 0; i < m - 1; i++) {
+        prime *= PRIME;
+    }
+
+    for (int i = 0; i <= n - m; i++) {
+        if (text_hash == pattern_hash) {
+            return true;
+        }
+        if (i < n - m) {
+            text_hash = (text_hash - prime * (text[i] - 'a' + 1)) * PRIME + (text[i + m] - 'a' + 1);
         }
     }
-    return substrings.size();
+    return false;
 }
+
 
 int main() {
     int n; cin >> n;
     string arr[n];
+
+    int min_size = 1e5;
+    string min_string;
+
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        string s; cin >> s;
+        if (min_size > s.length()) {
+            min_size = s.length();
+            min_string = s;
+        }
+        arr[i] = s;
     }
 
+    int max_size = 0;
+    string lcs = "";
+
+    for (int i = 0; i < min_size; i++) {
+        for (int j = 1; j <= min_size - i; j++) {
+            string substr = min_string.substr(i, j);
+            bool flag = true;
+
+            for (int k = 0; k < n; k++) {
+                if (!rabinKarp(arr[k], substr)) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                if (max_size < substr.length()) {
+                    max_size = substr.length();
+                    lcs = substr;
+                }
+            }
+        }
+    }
+
+    cout << lcs;
 }
